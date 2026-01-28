c7a0f7819afb84ead97a826a8b7f99b1ad76260b
Arpit Singh <thakurarpitsingh524@gmail.com>
2026-01-28 13:55:42 +0530

    update files

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='Templates')
 @app.route('/app')
 def index():
     return render_template("index.html")
